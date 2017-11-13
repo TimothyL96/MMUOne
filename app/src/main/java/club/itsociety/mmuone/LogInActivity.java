d62a0a0e962f37f0b2fbc5394a1cb00aa222bc9e
ttimt <geroginalam@hotmail.com>
2017-11-13 09:02:15 +0800

    removed unused import

--- app/src/main/java/club/itsociety/mmuone/LogInActivity.java
+++ app/src/main/java/club/itsociety/mmuone/LogInActivity.java
@@ -12,7 +12,6 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
-import android.text.LoginFilter;
 import android.text.TextWatcher;
 import android.view.MotionEvent;
 import android.view.View;
